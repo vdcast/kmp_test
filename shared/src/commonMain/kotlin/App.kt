f1ce65280f81d35a678af0d065c19f8ffcb516e0
vdcast <vdfree448@protonmail.com>
2023-09-28 13:17:43 +0300

    done

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,33 +1,164 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import model.BirdImage
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalResourceApi::class)
+
+@Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Magenta),
+        shapes = MaterialTheme.shapes.copy(
+            small = RoundedCornerShape(16.dp),
+            medium = RoundedCornerShape(16.dp),
+            large = RoundedCornerShape(16.dp)
+        )
+    ) {
+        content()
+    }
+}
+
 @Composable
 fun App() {
+    BirdAppTheme {
+        val birdsViewModel = getViewModel(Unit, viewModelFactory { BirdsViewModel() })
+        BirdsPage(viewModel = birdsViewModel)
+    }
+}
+//@Composable
+//fun App() {
+//    MaterialTheme {
+//        val birdsViewModel = getViewModel(Unit, viewModelFactory { BirdsViewModel() })
+//        BirdsPage(viewModel = birdsViewModel)
+//    }
+//}
+
+@Composable
+fun BirdsPage(viewModel: BirdsViewModel) {
+
+    val uiState by viewModel.uiState.collectAsState()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            for (category in uiState.categories) {
+                Button(
+                    onClick = {
+                        viewModel.selectCategory(category = category)
+                    },
+                    modifier = Modifier.aspectRatio(1.0f).fillMaxSize().weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 8.dp,
+                        pressedElevation = 8.dp
+                    )
+                ) {
+                    Text(text = category)
+                }
+            }
+        }
+
+
+        AnimatedVisibility(uiState.selectedImages.isNotEmpty()) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 5.dp),
+                content = {
+                    items(uiState.selectedImages) {
+                        BirdImageCell(it)
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun BirdImageCell(image: BirdImage) {
+    KamelImage(
+        asyncPainterResource("https://sebi.io/demo-image-api/${image.path}"),
+        contentDescription = "${image.category} by ${image.author}",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1.0f)
+    )
+}
+
+
+@Composable
+fun AppOld() {
     MaterialTheme {
         var greetingText by remember { mutableStateOf("Hello, World!") }
         var showImage by remember { mutableStateOf(false) }
+
+//        val listOfBirdsState = remember { mutableStateOf(listOf<BirdImage>()) }
+
+        LaunchedEffect(key1 = Unit) {
+//            listOfBirdsState.value = getImages()
+//            println(getImages())
+        }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
-            }) {
+            Button(
+                onClick = {
+                    greetingText = "Hello, ${getPlatformName()}"
+                    showImage = !showImage
+                }
+            ) {
                 Text(greetingText)
             }
             AnimatedVisibility(showImage) {
@@ -40,8 +171,18 @@ fun App() {
 //                    null
 //                )
             }
+
+//            LazyColumn {
+//                itemsIndexed(listOfBirdsState.value) { index, item ->
+//                    Text(item.category)
+//                    Text(item.path)
+//                }
+//            }
+
+
         }
     }
 }
+
 
 expect fun getPlatformName(): String
